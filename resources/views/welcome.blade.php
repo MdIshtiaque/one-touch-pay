c85040676445587efb6530cd2a3c79b80aec0ec7
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2024-02-18 11:01:17 +0600

    ui update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>One Touch Pay - Seamless Payments</title>
     @include('partial.style')
 </head>
-<body class="font-sans" style="background-color: lavender; padding-bottom: 100px">
+<body class="font-sans" style="background-color: lavender;">
 
 <!-- Navbar -->
 @include('partial.navbar')
